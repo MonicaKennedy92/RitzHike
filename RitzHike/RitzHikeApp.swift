52a6ab8e0528c96e7f423c8fa5992a9cb576397d
MonicaKennedy92 <monicakennedyofficial@gmail.com>
2024-08-13 15:19:26 -0600

    Initial Commit

---
+++ RitzHike/RitzHikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RitzHikeApp.swift
+//  RitzHike
+//
+//  Created by Monica on 2024-08-13.
+//
+
+import SwiftUI
+
+@main
+struct RitzHikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
