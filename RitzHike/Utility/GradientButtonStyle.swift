538e1106ae199b31b15c56a3fffcbc1b7dd75827
MonicaKennedy92 <monicakennedyofficial@gmail.com>
2024-08-15 13:08:57 -0600

    Added custom Button Style and change the random image while clicking the button with small animation

---
+++ RitzHike/Utility/GradientButtonStyle.swift
@@ -0,0 +1,25 @@
+//
+//  GradientButtonStyle.swift
+//  RitzHike
+//
+//  Created by Monica on 2024-08-15.
+//
+
+import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                configuration.isPressed ?
+                LinearGradient(colors: [.customGrayMedium, .customGrayLight], startPoint: .top, endPoint: .bottom)
+                :
+                LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+            )
+            .cornerRadius(40)
+    }
+}
