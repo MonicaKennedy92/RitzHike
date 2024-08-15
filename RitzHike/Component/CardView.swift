dfda03ee103bf582fc2e83140748392614d06cfc
MonicaKennedy92 <monicakennedyofficial@gmail.com>
2024-08-15 12:41:09 -0600

    Created header section for the card view

--- RitzHike/Component/CardView.swift
+++ RitzHike/Component/CardView.swift
@@ -13,24 +13,58 @@ struct CardView: View {
             
             CustomBackgroundView()
             
-            ZStack {
+            VStack {
+              //MARK: - Header
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                        .foregroundStyle(LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                        
+                        Spacer()
+                        
+                        Button {
+                            print("button was pressed")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+                    
+                    Text("Fun and enjouable outdoor activity for friends and families.")
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
                 
-                Circle()
-                    //.fill(.indigo)
-                    .fill(
-                        LinearGradient(
-                            colors: [
-                                Color("ColorIndigoMedium"),
-                                Color("ColorSalmonLight")],
-                                startPoint: .topLeading,
-                                endPoint: .bottomTrailing
+                // MARK: - Main Content
+                
+                
+                
+                
+                
+                ZStack {
+                    
+                    Circle()
+                        //.fill(.indigo)
+                        .fill(
+                            LinearGradient(
+                                colors: [
+                                    Color("ColorIndigoMedium"),
+                                    Color("ColorSalmonLight")],
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing
+                            )
                         )
-                    )
-                    .frame(width: 256, height: 256)
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                         .resizable()
+                     .scaledToFit()
+                }
+                // MARK: - Footer
                 
-                Image("image-1")
-                     .resizable()
-                 .scaledToFit()
             }
         }
         .frame(width: 320, height: 570)
